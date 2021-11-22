bd488bc921b4c07a652e1d6ed417618556f7b561
David Sola <david.sola1991@gmail.com>
2021-11-22 19:58:13 +0100

    Adding action

--- Bipedal_Walker.py
+++ Bipedal_Walker.py
@@ -12,10 +12,14 @@ import gym
 from TD3_Agent import Agent
 import matplotlib.pyplot as plt
 from collections import deque
+import numpy as np
+import random
 
+def normalize(data, min_value, max_value):
 
+        return ((data - min_value) / (max_value - min_value))
 # Change betweend Hardcore or non Hardcore version
-hc = 0
+hc = 1
 
 if hc==1:
     env = gym.make('BipedalWalkerHardcore-v3')
@@ -35,10 +39,13 @@ max_episodes = 10000
 episode_range = 300
 
 # How many episodes random actions shall be taken
-n_rand_actions = 100
+n_rand_actions = 25
 
 # Best reward from all episodes
 best_reward = -999
+
+max_pos_reward = 1e-7
+max_neg_reward = 1e-7
 
 # Empty lists for x and y values to be able to plot the accumulated reward after
 # the episodes
@@ -51,7 +58,8 @@ seed = 0
 env.seed(seed)
 
 # Sigma value for exploration
-sigma = 0.1
+sigma = 0.7
+sigma_decay = 0.997
 
 # Counter for number of interactions withe nvironemtn
 total_int = 0
@@ -61,31 +69,51 @@ agent = Agent(24, 4, random_seed=2)
 
 ''' START OF THE WHOLE TRAINING LOOP '''
 for i_episode in range(max_episodes):
-
+    sigma = max(sigma*sigma_decay, 0.0)
     accumulated_reward = 0
-
+    last_action = env.action_space.sample()
     # Get the first state from the environment
     state = env.reset()
-   
+    last_state = state
+    nr_of_its = 0
+    target_actor = random.uniform(0,1)
     ''' START OF THE TRAINING LOOP FOR EACH EPISODE '''
     for t in range(episode_range):
-        
+        nr_of_its = t
         total_int += 1
         # Take an action with the agent with added noise in the current state
         # For the first n episodes take random actions
         if i_episode < n_rand_actions:
             action = env.action_space.sample()
         else:
-            action = agent.act_noise(state, 0)    
-        
+            action = agent.act_noise(state, last_state, last_action, sigma, target_actor)    
         # Get the feedback from the environment by performing the action
         next_state, reward, done, info = env.step(action)
+        accumulated_reward = accumulated_reward + reward
+        if np.sign(reward)==1:
+            if reward > max_pos_reward:
+                corr_factor = reward/max_pos_reward
+                print('Pos_CorR_Factor: ', corr_factor)
+                for i in range(min(agent.memory.mem_cntr, agent.batch_size)):
+                    if agent.memory.reward_memory[i] > 0:
+                        agent.memory.reward_memory[i] /= corr_factor 
+                max_pos_reward = reward
+            reward = normalize(reward, 0, max_pos_reward)
+        else:
+            if np.abs(reward) > max_neg_reward:
+                corr_factor = np.abs(reward)/max_neg_reward
+                print('Neg_CorR_Factor: ', corr_factor)
+                for i in range(min(agent.memory.mem_cntr, agent.batch_size)):
+                    if agent.memory.reward_memory[i] > 0:
+                        agent.memory.reward_memory[i] /= corr_factor
+                max_neg_reward = np.abs(reward)
+            reward = -normalize(np.abs(reward), 0, max_neg_reward) * 10
            
         # Accumulate the reward to get the reward at the end of the episode
-        accumulated_reward = accumulated_reward + reward
+        
         
         # Add the state, action, next_state, reward transition into the replay buffer
-        agent.add_memory(state, action, reward, next_state, done)
+        agent.add_memory(state, action, reward, next_state, done, last_action, last_state)
         
         # Take a step with the agent in order to learn but collect first 
         # sufficient amount of data
@@ -93,22 +121,31 @@ for i_episode in range(max_episodes):
             agent.learn()
         
         # Assign the next state to the current state
+        last_state = state
         state = next_state
+        last_action = action
         
         # If the episode is done finish the loop
         if done:
             break
             
-    
+    accumulated_reward = max(accumulated_reward, -500)
     # Print some usefull stuff
     print('Nr of ints: ', total_int, ' Episode ', i_episode, ' reward: ', '{:.2f}'.format(accumulated_reward), \
                                         ' 100 episode avrg: ',  '{:.2f}'.format(sum(last_100_average)/100), \
-                                        'Best reward: ' , '{:.2f}'.format(best_reward))
+                                        'Best reward: ' , '{:.2f}'.format(best_reward), \
+                                        'Sigma: ', '{:.2f}'.format(sigma))
     last_100_average.appendleft(accumulated_reward)
-    x.append(i_episode)
-    y.append(sum(last_100_average)/100)
+    #x.append(i_episode)
+    #y.append(agent.accumulated_loss/nr_of_its)
+    agent.accumulated_loss = 0
     if accumulated_reward > best_reward:
         best_reward = accumulated_reward
+        agent.best_act_available = 1
+        if target_actor > 0.5:
+            agent.soft_update(agent.actor_local, agent.best_actor, 1)
+        else:
+            agent.soft_update(agent.actor_local2, agent.best_actor, 1)
         
         if best_reward > 50:
             episode_range = 1000
@@ -121,17 +158,22 @@ for i_episode in range(max_episodes):
         nr_eval_episodes = 10
         for eval_episodes in range(nr_eval_episodes):
             state, done = env.reset(), False
+            last_action = env.action_space.sample()
+            last_state = state
             while not done:
-                action = agent.act(state) 
-                state, reward, done, info = env.step(action)
+                action = agent.act(state, last_state, last_action) 
+                next_state, reward, done, info = env.step(action)
+                last_state= state
+                state = next_state
                 average_rew += reward
+                last_action = action
         average_rew /= nr_eval_episodes
         
-        plt.pause(0.1)
-        plt.plot(x,y)
-        plt.title('Average 100 Episode Reward, Hardcore activated: '+ str(hc))
-        plt.pause(0.1)
-        plt.savefig('Reward.png')
+        #plt.pause(0.1)
+        #plt.plot(x,y)
+        #plt.title('Average 100 Episode Reward, Hardcore activated: '+ str(hc))
+        #plt.pause(0.1)
+        #plt.savefig('Reward.png')
 
         print("---------------------------------------")
         print('Evaluation over ', nr_eval_episodes, ' episodes. Average reward: ', average_rew, ' Hardcore activated: ', hc, ' Total Interactions: ', total_int)
@@ -139,3 +181,5 @@ for i_episode in range(max_episodes):
         
 
 env.close()
+
+
--- TD3_Agent.py
+++ TD3_Agent.py
@@ -11,7 +11,7 @@ Description:
 import numpy as np
 import random
 from collections import namedtuple, deque
-from model import Actor, Critic
+from model import Actor, Critic, ICM_module
 
 import torch
 import torch.nn as nn
@@ -19,19 +19,20 @@ import torch.nn.functional as F
 import torch.optim as optim
 import copy
 import datetime
+import random
 
 
 
-BUFFER_SIZE = int(1e7)  # replay buffer size
+BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 100      # minibatch size
 GAMMA = 0.98            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor
-LR_CRITIC = 5e-4       # learning rate of the critic
+LR_ACTOR = 6e-4         # learning rate of the actor
+LR_CRITIC = 6e-4       # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 POLICY_FREQ = 2
 NOISE_CLIP = 0.5
-POLICY_NOISE = 0.2
+POLICY_NOISE = 0.3
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -52,16 +53,28 @@ class Agent():
         self.tau=TAU
         self.gamma = GAMMA
         self.batch_size = BATCH_SIZE
+        self.accumulated_loss = 0
 
         # The actor network
         self.actor_local = Actor(state_space, action_space, out_fcn, fc1_units, fc2_units).to(device)
         self.actor_target = Actor(state_space, action_space, out_fcn, fc1_units, fc2_units).to(device)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=self.lr_actor)
 
+        self.actor_local2 = Actor(state_space, action_space, out_fcn, fc1_units, fc2_units).to(device)
+        self.actor_target2 = Actor(state_space, action_space, out_fcn, fc1_units, fc2_units).to(device)
+        self.actor_optimizer2 = optim.Adam(self.actor_local2.parameters(), lr=self.lr_actor)
+
+        self.best_actor = Actor(state_space, action_space, out_fcn, fc1_units, fc2_units).to(device)
+        self.best_act_available = 0
+
         # The critic network
         self.critic_local = Critic(state_space, action_space, out_fcn, fc1_units, fc2_units).to(device)
         self.critic_target = Critic(state_space, action_space, out_fcn, fc1_units, fc2_units).to(device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=self.lr_critic, weight_decay=WEIGHT_DECAY)
+
+        #The ICM module
+        self.reward_predictor = ICM_module(state_space, action_space, out_fcn, fc1_units, fc2_units).to(device)
+        self.reward_predictor_optimizer = optim.Adam(self.reward_predictor.parameters(), lr=1e-5)
         
         # Replay memory
         self.memory = ReplayBuffer(BUFFER_SIZE, [state_space], action_space)
@@ -77,11 +90,14 @@ class Agent():
 
         self.t_step = 0
 
-    def add_memory(self, state, action, reward, next_state, done):
+        self.actor_list = []
+        self.critic_list = []
+
+    def add_memory(self, state, action, reward, next_state, done, last_action, last_state):
         '''
         Add memories to the replay Bugger
         '''
-        self.memory.store_transition(state, action, reward, next_state, done)
+        self.memory.store_transition(state, action, reward, next_state, done, last_action, last_state)
         
     def save_network(self):
 
@@ -103,7 +119,7 @@ class Agent():
             self.critic_local.load_state_dict(torch.load(cr_loc))
             self.critic_target.load_state_dict(torch.load(cr_tar))
 
-    def act(self, state):
+    def act(self, state, last_state, last_action):
         '''
         Functionality to determine the action in the current state.
         Additionally to the current action which is determined by the actor a normal distribution will be added to the
@@ -114,14 +130,16 @@ class Agent():
         :return: action which shall be performed in the environment
         '''
         state = torch.from_numpy(state).float().to(device)
+        last_action = torch.from_numpy(last_action).float().to(device)
+        last_state = torch.from_numpy(last_state).float().to(device)
         self.actor_local.eval()
         with torch.no_grad():
-            action = self.actor_local(state).cpu().data.numpy()
+            action = self.actor_local.evaluate(state, last_state, last_action).cpu().data.numpy()
         self.actor_local.train()
         
         return np.clip(action, -1, 1)
 
-    def act_noise(self, state, sigma):
+    def act_noise(self, state, last_state, last_action, sigma, target_actor):
         '''
         Functionality to determine the action in the current state.
         Additionally to the current action which is determined by the actor a normal distribution will be added to the
@@ -134,10 +152,17 @@ class Agent():
 
         noise = np.random.normal(0, sigma, 4)
         state = torch.from_numpy(state).float().to(device)
+        last_action = torch.from_numpy(last_action).float().to(device)
+        last_state = torch.from_numpy(last_state).float().to(device)
         self.actor_local.eval()
-        with torch.no_grad():
-            action = self.actor_local(state).cpu().data.numpy()
-        self.actor_local.train()
+        if target_actor > 0.5:
+            with torch.no_grad():
+                action = self.actor_local.evaluate(state, last_state, last_action).cpu().data.numpy()
+            self.actor_local.train()
+        else:
+            with torch.no_grad():
+                action = self.actor_local2.evaluate(state, last_state, last_action).cpu().data.numpy()
+            self.actor_local2.train()
 
         action = action + noise
         
@@ -191,44 +216,199 @@ class Agent():
         if self.memory.mem_cntr < self.batch_size:
             return
         self.total_it += 1
-        state, action, reward, new_state, done = \
+        state, action, reward, new_state, done, last_action, last_state = \
                 self.memory.sample_buffer(self.batch_size)
+
+        if random.uniform(0,1) > 0.5:
+            train_first = 1
+        else:
+            train_first = 0
 
         rewards = torch.tensor(reward, dtype=torch.float).to(device)
         dones = torch.tensor(done, dtype=torch.float).to(device)
         next_states = torch.tensor(new_state, dtype=torch.float).to(device)
         states = torch.tensor(state, dtype=torch.float).to(device)
         actions = torch.tensor(action, dtype=torch.float).to(device)
+        last_actions = torch.tensor(last_action, dtype=torch.float).to(device)
+        last_states = torch.tensor(last_state, dtype=torch.float).to(device)
+
+        predicted_rewards = self.reward_predictor.predict_reward(states, actions)
+        prediction_loss = F.mse_loss(predicted_rewards.view(-1), rewards)
+        self.reward_predictor_optimizer.zero_grad()
+        prediction_loss.backward()
+        self.reward_predictor_optimizer.step()
+
+        
+        pol_loss = 0
+        pol_loss_ = 0
+        if self.total_it%2==0 and self.best_act_available==1:
+            best_actions = self.best_actor(states, last_states, actions)
+            policy_actions = self.actor_local(states, last_states, actions)
+
+            pol_loss = F.mse_loss(policy_actions, best_actions) * 30
         
         with torch.no_grad():  
+            # Get intrinsic reward
+
             # Select action according to policy and add clipped noise
-            noise = (torch.randn_like(actions) * POLICY_NOISE).clamp(-NOISE_CLIP, NOISE_CLIP)
-            next_actions = (self.actor_target(next_states) + noise).clamp(-1,1)
+            if self.total_it%2==0 and self.best_act_available==1:
+                noise = (torch.randn_like(actions) * POLICY_NOISE).clamp(-NOISE_CLIP, NOISE_CLIP)
+                next_actions = (self.best_actor(next_states, states, actions) + noise).clamp(-1,1)
+                target_Q1, target_Q2 = self.critic_target(next_states, next_actions, actions, last_states)
+                target_q = torch.min(target_Q1.view(-1), target_Q2.view(-1))
+
+            else:
+                noise = (torch.randn_like(actions) * POLICY_NOISE).clamp(-NOISE_CLIP, NOISE_CLIP)
+                next_actions_first = (self.actor_target(next_states, states, actions) + noise).clamp(-1,1)
+                next_actions_second = (self.actor_target2(next_states, states, actions) + noise).clamp(-1,1)
+                
+                # Compute the target Q value
+                target_Q1_first, target_Q2_first = self.critic_target(next_states, next_actions_first, actions, last_states)
+                if train_first == 1:
+                    target_q_first = torch.min(target_Q1_first.view(-1), target_Q2_first.view(-1))
+                else:
+                    target_q_first = torch.max(target_Q1_first.view(-1), target_Q2_first.view(-1))
+                target_Q1_second, target_Q2_second = self.critic_target(next_states, next_actions_second, actions, last_states)
+                if train_first == 1:
+                    target_q_second = torch.min(target_Q1_second.view(-1), target_Q2_second.view(-1))
+                else:
+                    target_q_second = torch.max(target_Q1_second.view(-1), target_Q2_second.view(-1))
+
+                target_q = torch.max(target_q_first, target_q_second)
+                
+
+
+            target_q = rewards +(self.gamma * target_q * (1 - dones))
              
-            # Compute the target Q value
-            target_Q1, target_Q2 = self.critic_target(next_states, next_actions)
-            target_q = torch.min(target_Q1.view(-1), target_Q2.view(-1))
-            target_q = rewards + (self.gamma * target_q * (1 - dones))
-             
-        current_Q1, current_Q2 = self.critic_local(states, actions)
+        current_Q1, current_Q2 = self.critic_local(states, actions, last_actions, last_states)
         critic_loss = F.mse_loss(current_Q1.view(-1), target_q) + F.mse_loss(current_Q2.view(-1), target_q)
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
-        ''' GRADIENT CLIPPING TO BE EVALUATED!!'''
-        #torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(),1)
         self.critic_optimizer.step()
-        
+
+        critic_copy = copy.deepcopy(self.critic_local)
+        critic_copy_random = copy.deepcopy(self.critic_local)
+        self.soft_update_random(critic_copy_random, critic_copy_random, self.tau)
+        self.critic_list.append(critic_copy)
+        self.critic_list.append(critic_copy_random)
+        if len(self.critic_list) > 20:
+            self.sample_best_critic()
+        '''
+        predicted_state = self.icm.predict_state(states, actions)
+        icm_loss = F.mse_loss(next_states, predicted_state)
+        icm_loss_actor = icm_loss.detach().clone()
+        self.icm_optimizer.zero_grad()
+        icm_loss.backward()
+        self.icm_optimizer.step()
+        '''
+
+        actor_loss = 0
         if self.total_it % POLICY_FREQ == 0:
             #Compute actor loss
-            actor_loss = -self.critic_local.Q1(states, self.actor_local(states)).mean()
-            
-            #Optimize the actor
-            self.actor_optimizer.zero_grad()
-            actor_loss.backward()
-            self.actor_optimizer.step()
-            self.soft_update(self.critic_local, self.critic_target, self.tau)
-            self.soft_update(self.actor_local, self.actor_target, self.tau)
+            if train_first == 1:
+                actor_loss = -self.critic_local.Q1(states, self.actor_local(states, last_states, last_actions), last_actions, last_states).mean() + pol_loss
+                    #Optimize the actor
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+                self.soft_update(self.critic_local, self.critic_target, self.tau)
+                self.soft_update(self.actor_local, self.actor_target, self.tau)
+
+                actor_copy = copy.deepcopy(self.actor_local)
+                actor_copy_random = copy.deepcopy(self.actor_local)
+                self.soft_update_random(actor_copy, actor_copy_random, self.tau)
+
+                self.actor_list.append(actor_copy)
+                self.actor_list.append(actor_copy_random)
+            else:
+                actor_loss = -self.critic_local.Q1(states, self.actor_local2(states, last_states, last_actions), last_actions, last_states).mean() + pol_loss
+                #Optimize the actor
+                self.actor_optimizer2.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer2.step()
+                self.soft_update(self.critic_local, self.critic_target, self.tau)
+                self.soft_update(self.actor_local2, self.actor_target2, self.tau)
+
+                actor_copy = copy.deepcopy(self.actor_local2)
+                actor_copy_random = copy.deepcopy(self.actor_local2)
+                self.soft_update_random(actor_copy, actor_copy_random, self.tau)
+
+                self.actor_list.append(actor_copy)
+                self.actor_list.append(actor_copy_random)
+
         
+        if len(self.actor_list) > 20:
+            self.sample_best_actor()
+           
+
+        
+        self.accumulated_loss += actor_loss
+
+    def sample_best_actor(self):
+        if self.memory.mem_cntr < self.batch_size*5:
+            return
+        state, _, _, new_state, _, _, last_state = \
+                self.memory.sample_buffer(self.batch_size*5)
+
+        next_states = torch.tensor(new_state, dtype=torch.float).to(device)
+        states = torch.tensor(state, dtype=torch.float).to(device)
+        last_states = torch.tensor(last_state, dtype=torch.float).to(device)
+
+        best_reward = -999
+        number_of_winner = 0
+        for i in range(len(self.actor_list)):
+            current_action = self.actor_list[i].forward(states, last_states)
+            next_action = self.actor_list[i].forward(next_states, states)
+            current_reward = self.reward_predictor.predict_reward(states, current_action)
+            next_reward = self.reward_predictor.predict_reward(next_states, next_action)
+            target_Q1_first, target_Q2_first = self.critic_target(next_states, next_action, current_action, last_states)
+            target_q_first = torch.min(target_Q1_first.view(-1), target_Q2_first.view(-1))
+            target_q = current_reward +(self.gamma * target_q_first)
+
+            reward_sum = torch.sum(target_q) + torch.sum(next_reward)
+
+            if reward_sum > best_reward:
+                number_of_winner = i
+                best_reward = reward_sum
+
+        #print('Winner was: ', number_of_winner)
+        best_actor = self.actor_list[number_of_winner]
+        self.soft_update(self.actor_list[number_of_winner], self.actor_local2, 0.01)
+        self.soft_update(self.actor_list[number_of_winner], self.actor_local, 0.01)
+        self.soft_update(self.actor_list[number_of_winner], self.actor_target, 0.005)
+        self.soft_update(self.actor_list[number_of_winner], best_actor, 1)
+        self.actor_list = []
+        #self.actor_list.append(best_actor)
+
+    def sample_best_critic(self):
+        if self.memory.mem_cntr < self.batch_size*5:
+            return
+        state, action, reward, new_state, done, last_action, last_state = \
+                self.memory.sample_buffer(self.batch_size*5)
+
+        next_states = torch.tensor(new_state, dtype=torch.float).to(device)
+        states = torch.tensor(state, dtype=torch.float).to(device)
+        last_states = torch.tensor(last_state, dtype=torch.float).to(device)
+        actions = torch.tensor(action, dtype=torch.float).to(device)
+        last_actions = torch.tensor(last_action, dtype=torch.float).to(device)
+
+        best_reward = -999
+        for i in range(len(self.critic_list)):
+            current_Q1, current_Q2 = self.critic_list[i].forward(states, actions, last_actions, last_states)
+            reward_sum = torch.sum(torch.max(current_Q1, current_Q2))
+
+            if reward_sum > best_reward:
+                number_of_winner = i
+                best_reward = reward_sum
+
+        #print('Winner was: ', number_of_winner)
+        best_critic = self.critic_list[number_of_winner]
+        self.soft_update(self.critic_list[number_of_winner], self.critic_local, 0.01)
+        self.soft_update(self.critic_list[number_of_winner], self.critic_target, 0.05)
+        self.critic_list = []
+        #self.critic_list.append(best_critic)
+
+
 
     def exp_lr_scheduler(self, optimizer, decayed_lr):
         '''
@@ -253,6 +433,17 @@ class Agent():
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0 - tau)*target_param.data)
 
+    def soft_update_random(self, local_model, target_model, tau):
+        '''
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+        :param local_mode:
+        :param target_model:
+        :param tau:
+        :return:
+        '''
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(local_param.data + 0.001*torch.randn(local_param.data.size()).to(device))
+
 class ReplayBuffer():
     def __init__(self, max_size, input_shape, n_actions):
         self.mem_size = max_size
@@ -262,14 +453,18 @@ class ReplayBuffer():
         self.action_memory = np.zeros((self.mem_size, n_actions))
         self.reward_memory = np.zeros(self.mem_size)
         self.terminal_memory = np.zeros(self.mem_size)
+        self.last_action = np.zeros((self.mem_size, n_actions))
+        self.last_state = np.zeros((self.mem_size, *input_shape))
 
-    def store_transition(self, state, action, reward, state_, done):
+    def store_transition(self, state, action, reward, state_, done, last_action, last_state):
         index = self.mem_cntr % self.mem_size
         self.state_memory[index] = state
         self.action_memory[index] = action
         self.reward_memory[index] = reward
         self.new_state_memory[index] = state_
         self.terminal_memory[index] = done
+        self.last_action[index] = last_action
+        self.last_state[index] = last_state
 
         self.mem_cntr += 1
 
@@ -283,8 +478,10 @@ class ReplayBuffer():
         rewards = self.reward_memory[batch]
         states_ = self.new_state_memory[batch]
         dones = self.terminal_memory[batch]
+        last_action = self.last_action[batch]
+        last_state = self.last_state[batch]
 
-        return states, actions, rewards, states_, dones
+        return states, actions, rewards, states_, dones, last_action, last_state
     
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
